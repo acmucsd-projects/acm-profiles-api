17d7ca7a3abc59d116155001ba413e8416b25b05
Shravan Konduru <42595673+shravan1k@users.noreply.github.com>
2020-11-14 19:19:10 -0800

    Removed errors from models

--- profiles/models.py
+++ profiles/models.py
@@ -8,28 +8,29 @@ class Profiles(models.Model):
     last_name = models.CharField(max_length = 255)
     major = models.CharField(max_length = 255)
     grad_year = models.IntegerField()
-    profile_pic = models.CharField(blank = true, null = true, max_length = 255)
+    profile_pic = models.CharField(blank = True, null = True, max_length = 255)
 
 class Settings(models.Model):
-    uuid = models.CharField(primary_key = True, max_length = 255, unique = True)
+    uuid = models.ForeignKey(Profiles, on_delete=models.CASCADE, related_name='settings')
     profile_visibility = models.BooleanField(default = True)
     follower_visibility = models.BooleanField(default = True)
     following_visibility = models.BooleanField(default = True)
 
-class User_Socials(models.Model):
+class User_following(models.Model):
     follower = models.CharField(max_length = 255)
     following = models.CharField(max_length = 255)
 
 class Communities(models.Model):
+    ucid = models.CharField(primary_key = True, max_length = 255, unique = True)
     title = models.CharField(max_length=255)
-    description = models.TextField(s)
-    profile_image_link = models.CharField()
+    description = models.TextField()
+    profile_image_link = models.CharField(max_length=255)
     active = models.BooleanField(max_length=255)
 
 class Community_members(models.Model):
     ucid = models.ForeignKey(Communities, on_delete=models.CASCADE, related_name='members')
     member_id = models.ForeignKey(Profiles, on_delete=models.CASCADE, related_name='communities')
-    admin = models.BooleanField()
+    admin = models.BooleanField(default = False)
 
 class Community_socials(models.Model):
     ucid = models.ForeignKey(Communities, on_delete=models.CASCADE, related_name='socials')
@@ -38,7 +39,7 @@ class Community_socials(models.Model):
     instagram = models.CharField(max_length=255)
 
 class User_socials(models.Model):
-    uuid = models.ForeignKey(Profiles, on_delete=models.CASCADE, related_name='communities')
+    uuid = models.ForeignKey(Profiles, on_delete=models.CASCADE, related_name='socials')
     discord = models.CharField(max_length=255)
     facebook = models.CharField(max_length=255)
     snapchat = models.CharField(max_length=255)
