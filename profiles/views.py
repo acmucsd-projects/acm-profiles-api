50d88973e08999f7e68f59bd80460daf2c2babff
Shravan Konduru <42595673+shravan1k@users.noreply.github.com>
2020-11-30 13:00:15 -0800

    added admin authentication for editing/deleting communities

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from rest_framework import generics, filters, mixins
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from .models import *
@@ -46,15 +47,7 @@ class FollowingView_RD(generics.RetrieveDestroyAPIView):
     queryset = User_following.objects.all()
     serializer_class = FollowingSerializer
 
-class Community_MembershipView(generics.ListAPIView):
-    queryset = Community_members.objects.all()
-    serializer_class = Community_MembershipSerializer
-    def get_queryset(self):
-        uuid = self.kwargs.get("member")
-        queryset = Community_members.objects.filter(member_id=uuid)
-        return queryset
-
-class CommunitiesView_LC(generics.ListCreateAPIView):
+class CommunitiesSearchView(generics.ListAPIView):
     queryset = Communities.objects.all()
     serializer_class = CommunitiesSerializer
     filter_backends = [filters.SearchFilter]
@@ -66,9 +59,41 @@ class CommunitiesView_LC(generics.ListCreateAPIView):
             queryset = queryset.filter(active=visibility)
         return queryset
 
-class CommunitiesView_RUD(generics.RetrieveUpdateDestroyAPIView):
+class CommunitiesCreateView(generics.CreateAPIView):
     queryset = Communities.objects.all()
     serializer_class = CommunitiesSerializer
+
+class CommunitiesView_UD(generics.UpdateDestroyAPIView):
+    queryset = Communities.objects.all()
+    serializer_class = CommunitiesSerializer
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+        queryset2 = Community_members.objects.all()
+        filter = {}
+        filter2 = {}
+        for field in self.kwargs:
+            if (field == "ucid"):
+                filter[field] = self.kwargs[field]
+        for field in self.kwargs:
+            if self.kwargs[field]:
+                if field == "ucid":
+                    filter2[field] = self.kwargs[field]
+                else:
+                    filter2["member_id"] = self.kwargs[field]
+        admin_obj = get_object_or_404(queryset2, **filter2)
+        if admin_obj.admin:  
+            obj = get_object_or_404(queryset, **filter)
+            self.check_object_permissions(self.request, obj)
+            return obj
+        raise Http404("User is not an admin")
+
+class Community_MembershipView(generics.ListAPIView):
+    queryset = Community_members.objects.all()
+    serializer_class = Community_MembershipSerializer
+    def get_queryset(self):
+        uuid = self.kwargs.get("member")
+        queryset = Community_members.objects.filter(member_id=uuid)
+        return queryset
 
 class CommunityMemberView_LC(generics.ListCreateAPIView):
     queryset = Community_members.objects.all()
