88a48d5a50a792ce22b65519c6dff1ee76496d51
Bryce Tsuyuki <btsuyuki1@gmail.com>
2020-12-08 22:58:22 -0800

    Lowercase cut command and remove extraneous env vars in travis config

--- env_setup.sh
+++ env_setup.sh
@@ -1,7 +1,13 @@
 #STR="postgres://user:password@host:port/database_name"
 TEMP=$(echo $DATABASE_URL | cut -c12-)
+echo $DATABASE_URL
 export DB_NAME=$(echo $TEMP | cut -d':' -f 1)
-export DB_PASSWORD=$(echo $TEMP | CUT -d':' -f 2 | CUT -d'@' -f 1)
-export DB_HOST=$(echo $TEMP | CUT -d':' -f 2 | CUT -d'@' -f 2)
-export DB_PORT=$(echo $TEMP | CUT -d':' -f 3 | CUT -d'/' -f 1)
-export DB_NAME=$(echo $TEMP | CUT -d':' -f 3 | CUT -d'/' -f 2)
+echo $DB_NAME
+export DB_PASSWORD=$(echo $TEMP | cut -d':' -f 2 | cut -d'@' -f 1)
+echo $DB_PASSWORD
+export DB_HOST=$(echo $TEMP | cut -d':' -f 2 | cut -d'@' -f 2)
+echo $DB_HOST
+export DB_PORT=$(echo $TEMP | cut -d':' -f 3 | cut -d'/' -f 1)
+echo $DB_PORT
+export DB_NAME=$(echo $TEMP | cut -d':' -f 3 | cut -d'/' -f 2)
+echo $DB_NAME
