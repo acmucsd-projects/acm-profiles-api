1cd7f8b69dc9ab4819ac4a05b7217349f5989e17
Kunal Arora <kuarora@ucsd.edu>
2020-12-08 22:18:29 -0800

    script for env vars

---
+++ env_setup.sh
@@ -0,0 +1,8 @@
+STR="postgres://user:password@host:port/database_name"
+TEMP=$(echo $STR | cut -c12-)
+export DB_NAME=$(echo $TEMP | cut -d':' -f 1)
+export DB_PASSWORD=$(echo $TEMP | CUT -d':' -f 2 | CUT -d'@' -f 1)
+export DB_HOST=$(echo $TEMP | CUT -d':' -f 2 | CUT -d'@' -f 2)
+export DB_PORT=$(echo $TEMP | CUT -d':' -f 3 | CUT -d'/' -f 1)
+export DB_NAME=$(echo $TEMP | CUT -d':' -f 3 | CUT -d'/' -f 2)
+
