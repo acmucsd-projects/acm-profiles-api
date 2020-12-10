21b9c4aa847156f1eedb7c78f91926f1ca77fbc5
Bryce Tsuyuki <btsuyuki1@gmail.com>
2020-12-09 16:39:11 -0800

    Switch to using DOCKER_ prefix for docker env vars

--- env-setup.sh
+++ env-setup.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 TEMP=$(echo $DATABASE_URL | cut -c12-)
-export DB_USER=$(echo $TEMP | cut -d':' -f 1)
-export DB_PASSWORD=$(echo $TEMP | cut -d':' -f 2 | cut -d'@' -f 1)
-# export DB_HOST=$(echo $TEMP | cut -d':' -f 2 | cut -d'@' -f 2)
-export DB_PORT=$(echo $TEMP | cut -d':' -f 3 | cut -d'/' -f 1)
-export DB_NAME=$(echo $TEMP | cut -d':' -f 3 | cut -d'/' -f 2)
+export DOCKER_DB_USER=$(echo $TEMP | cut -d':' -f 1)
+export DOCKER_DB_PASSWORD=$(echo $TEMP | cut -d':' -f 2 | cut -d'@' -f 1)
+export DOCKER_DB_PORT=$(echo $TEMP | cut -d':' -f 3 | cut -d'/' -f 1)
+export DOCKER_DB_NAME=$(echo $TEMP | cut -d':' -f 3 | cut -d'/' -f 2)
