e78859b79cc7a21fd517505b9bbc8452a02582c3
Shravan Konduru <42595673+shravan1k@users.noreply.github.com>
2020-12-16 17:49:46 -0800

    fixed error with getting uuid

--- profiles/views.py
+++ profiles/views.py
@@ -30,7 +30,7 @@ class ProfileViewSet(ModelViewSet):
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
-        uuid = request.data.get("uuid")
+        uuid = serializer.data.get("uuid")
         user = Profiles.objects.get(uuid=uuid)
         settings = Settings(user=user)
         settings.save()
