dbcc5240d2ab43d83822a6979691984e0c471c4e
Shravan Konduru <42595673+shravan1k@users.noreply.github.com>
2020-12-29 21:31:42 -0800

    fix error

--- acmprofiles/settings.py
+++ acmprofiles/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if (os.environ["DEBUG"] == "True"):
+if os.environ["DEBUG"] == "True":
     DEBUG = True
 else:
     DEBUG = False
